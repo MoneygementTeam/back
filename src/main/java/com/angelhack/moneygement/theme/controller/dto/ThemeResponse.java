30c104c7b7e42e22fa2da42983d22ff79578e9e9
Tina <migujeong@gmail.com>
2024-08-25 03:59:11 +0900

    ✨ 테마별 수익률 표기 기능 추가

---
+++ src/main/java/com/angelhack/moneygement/theme/controller/dto/ThemeResponse.java
@@ -0,0 +1,12 @@
+package com.angelhack.moneygement.theme.controller.dto;
+
+public record ThemeResponse(
+	 String themeId,
+	 String themeName,
+	 String themeDesc,
+	 String commodityReturn,
+	 String coinReturn,
+	 String stockReturn,
+	 String realEstateReturn
+) {
+}
